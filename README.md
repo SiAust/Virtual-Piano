1989c05a85ea4184e5c8b97803fa613c70c354c4
Simon Aust <simon.aust@googlemail.com>
2020-07-01 11:39:46 +0100

    stage 4 complete

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ### Virtual Piano
-This is an example website that simulates a few
+This is an example website that simulates the A-J 
 keys on a piano.
 
 This will be built with HTML, CSS and Javascript.
+
+##### Changelist;
+- Sound added to keys
+- Gradient effect added when key pressed
+- Heading added, with Yellowtail font
--- index.js
+++ index.js
@@ -6,58 +6,121 @@ let keyG = document.getElementById("keyG");
 let keyH = document.getElementById("keyH");
 let keyJ = document.getElementById("keyJ");
 
+let firedA = false;
+let firedS = false;
+let firedD = false;
+let firedF = false;
+let firedG = false;
+let firedH = false;
+let firedJ = false;
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keypress", function (event) {
     if (event.key == "a") {
-        keyA.setAttribute("class", "active");
-        console.log("The '" + event.key + "' has been pressed.");
+        if (!firedA) {
+            firedA = true;
+            keyA.setAttribute("class", "active");
+            let audio = document.createElement("AUDIO");
+            audio.src = "./sounds/A.mp3";
+            audio.play();
+            console.log("The '" + event.key + "' has been pressed.");
+        }
+
     }
     if (event.key == "s") {
-        keyS.setAttribute("class", "active")
-        console.log("The '" + event.key + "' has been pressed.");
+        if (!firedS) {
+            firedS = true;
+            keyS.setAttribute("class", "active");
+            let audio = document.createElement("AUDIO");
+            audio.src = "./sounds/S.mp3";
+            audio.play();
+            console.log("The '" + event.key + "' has been pressed.");
+        }
+
     }
     if (event.key == "d") {
-        keyD.setAttribute("class", "active")
-        console.log("The '" + event.key + "' has been pressed.");
+        if (!firedD) {
+            firedD = true;
+            keyD.setAttribute("class", "active");
+            let audio = document.createElement("AUDIO");
+            audio.src = "./sounds/D.mp3";
+            audio.play();
+            console.log("The '" + event.key + "' has been pressed.");
+        }
+
     }
     if (event.key == "f") {
-        keyF.setAttribute("class", "active")
-        console.log("The '" + event.key + "' has been pressed.");
+        if (!firedF) {
+            firedF = true;
+            keyF.setAttribute("class", "active");
+            let audio = document.createElement("AUDIO");
+            audio.src = "./sounds/F.mp3";
+            audio.play();
+            console.log("The '" + event.key + "' has been pressed.");
+        }
+
     }
     if (event.key == "g") {
-        keyG.setAttribute("class", "active")
-        console.log("The '" + event.key + "' has been pressed.");
+        if (!firedG) {
+            firedG = true;
+            keyG.setAttribute("class", "active");
+            let audio = document.createElement("AUDIO");
+            audio.src = "./sounds/G.mp3";
+            audio.play();
+            console.log("The '" + event.key + "' has been pressed.");
+        }
+
     }
     if (event.key == "h") {
-        keyH.setAttribute("class", "active")
-        console.log("The '" + event.key + "' has been pressed.");
+        if (!firedH) {
+            firedH = true;
+            keyH.setAttribute("class", "active");
+            let audio = document.createElement("AUDIO");
+            audio.src = "./sounds/H.mp3";
+            audio.play();
+            console.log("The '" + event.key + "' has been pressed.");
+        }
+
     }
     if (event.key == "j") {
-        keyJ.setAttribute("class", "active")
-        console.log("The '" + event.key + "' has been pressed.");
+        if (!firedJ) {
+            firedJ = true;
+            keyJ.setAttribute("class", "active");
+            let audio = document.createElement("AUDIO");
+            audio.src = "./sounds/J.mp3";
+            audio.play();
+            console.log("The '" + event.key + "' has been pressed.");
+        }
+
     }
 })
 
 document.addEventListener("keyup", function (event) {
     if (event.key == "a") {
+        firedA = false;
         keyA.removeAttribute("class");
     }
     if (event.key == "s") {
+        firedS = false;
         keyS.removeAttribute("class");
     }
     if (event.key == "d") {
+        firedD = false;
         keyD.removeAttribute("class");
     }
     if (event.key == "f") {
+        firedF = false;
         keyF.removeAttribute("class");
     }
     if (event.key == "g") {
+        firedG = false;
         keyG.removeAttribute("class");
     }
     if (event.key == "h") {
+        firedH = false;
         keyH.removeAttribute("class");
     }
     if (event.key == "j") {
+        firedJ = false;
         keyJ.removeAttribute("class");
     }
 })
